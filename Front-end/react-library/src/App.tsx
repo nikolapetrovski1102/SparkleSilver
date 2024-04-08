23114b3aad2c9d7e58b0ad1279ca86170fed6d67
Stefan Todorovski <stefan.aplch@outlook.com>
2024-04-08 19:11:44 +0200

    static image added

--- Front-end/react-library/src/App.tsx
+++ Front-end/react-library/src/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 
 export const App = () => {
   return (
-
-    <div className='d-flex flex-column min-vh-100'>
-      <Navbar />
+    <div className='d-flex flex-column min-vh-100 background-image'>
       <div className='flex-grow-1'>
         <Switch>
-
           <Route path='/' exact>
             <Redirect to='/home' />
           </Route>
-
           <Route path='/home'>
             <HomePage />
           </Route>
-
         </Switch>
-
       </div>
-      <Footer />
     </div>
   );
 }
-
-
