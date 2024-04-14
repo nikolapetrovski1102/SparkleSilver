3acf312e660ff0d0f390682cb6fca0faa87f9764
Sani Radanlieva <radanlievas@gmail.com>
2024-04-14 02:02:55 +0200

    Created Home Page

--- Front-end/react-library/src/layouts/HomePage/HomePage.tsx
+++ Front-end/react-library/src/layouts/HomePage/HomePage.tsx
@@ -1,14 +1,39 @@
-
+import React from 'react';
+import Navbar from '../NavbarAndFooter/Navbar';
+import { NavLink } from "react-router-dom";
 
 export const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <main className="main-content">
+        <h1 className='homepage-collection' style={{ color: 'white' }}>Collection 2024</h1>
 
-return(
+              <div className="image-container">
+                  <div className="image-wrapper">
+                      <img src="https://media.tiffany.com/is/image/Tiffany/EcomBrowseM/-tiffany-hardwearsmall-wrap-necklace-38172794_1029509_ED.jpg?defaultImage=NoImageAvailableInternal" alt=" " />
+                      <NavLink to="/add-product" className="add-icon">➕</NavLink>
+                  </div>
+                  <div className="image-wrapper">
+                      <img src="https://www.uneakboutique.co.uk/cdn/shop/products/Sterling-Silver-Open-Sqiggle-Flower-Earrings-By-Elements-Silver_e895d93e-7c41-4029-946b-8fa5fc060dbd.jpg?v=1479321205" alt=" " />
+                      <NavLink to="/add-product" className="add-icon">➕</NavLink>
+                  </div>
+                  <div className="image-wrapper">
+                      <img src="https://www.lumije.com/cdn/shop/products/signature-14k-white-gold-four-prong-diamond-tennis-bracelet-h-colorsi-clarity-lumije-new-york-1.jpg?v=1654797342" alt=" " />
+                      <NavLink to="/add-product" className="add-icon">➕</NavLink>
+                   </div>
+              </div>
 
-    <>
-    
-    </>
+              <div className='add-product-container'>
+                  <div className='add-product'>
+                      <NavLink to="/add-product" className="add-product-link">+</NavLink>
+                      <h3>Додадете производ</h3>
+                  </div>
+    </div>
 
-);
+      </main>
+    </div>
+  );
+};
 
-
-}
+export default HomePage;
--- Front-end/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ Front-end/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,36 +1,29 @@
 import { NavLink } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = () => {
     return (
-
-        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-            <div className='container-fluid'>
-                <span className='navbar-brand'>E shop</span>
-                <button className='navbar-toggler' type='button'
-                    data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-                    aria-controls='navbarNavDropdown' aria-expanded='false'
-                    aria-label='Toggle Navigation'>
-                    <span className='navbar-toggler-icon'></span>
-                </button>
-                <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to="/home">Home</NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to='/search'>Search</NavLink>
-                        </li>
-                    </ul>
-                    <ul className='navbar-nav ms-auto'>
-                        <li className='nav-item m-1'>
-                            <a type='button' className='btn btn-outline-light' href='#'>Sign in</a>
-                        </li>
-                    </ul>
+        <>
+        <div className="navbar-container">
+            <div className="navbar-top">
+                <div className="navbar-logo">SparkleSilver</div>
+                <div className="navbar-right">
+                    <input type="search" placeholder="Пребарувајте" />
+                    <NavLink to="/profile" className="navbar-icon">👤</NavLink>
+                    <NavLink to="/cart" className="navbar-icon">🛒</NavLink>
                 </div>
             </div>
+            <nav className="navbar">
+                <ul className="nav-links">
+                    <li><NavLink to="/earrings">ОБЕТКИ</NavLink></li>
+                    <li><NavLink to="/necklaces">ОГРЛИЦИ</NavLink></li>
+                    <li><NavLink to="/bracelets">АЛКИ</NavLink></li>
+                    <li><NavLink to="/rings">ПРСТЕНИ</NavLink></li>
+                </ul>
         </nav>
+        </div>
 
+        </>
     );
+};
 
-
-}
+export default Navbar;
